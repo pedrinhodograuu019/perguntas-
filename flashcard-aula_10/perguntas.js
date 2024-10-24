b1999794c1fe0ac7f4fa9efa89d5fe32667ec985
pedrinhodograuu019 <00001079107782sp@al.educacao.sp.gov.br>
2024-10-24 08:06:57 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cantor',
+    'Quem foi michael jackson?',
+    'O Michael foi o cara do biri dim'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do brasil?',
+    'A capital do Brasil é Brasilía'
+)
+
+criaCartao(
+    'Qual o melhor jogo de Computador',
+    'O que é uma função?',
+    'O melhor jogo eletronico para computador é o League of Legends!!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz caixa de bom bom em Inglês?',
+    'caixa de bom bom em inglês é Box of Good Good!'
+)
